5a09a1d606b18e12d04ca7942d6f53f2318418de
Joshua Sebastian Kim <joshuakim150@gmail.com>
2017-09-25 18:06:23 -0300

    Removed console log

--- app/components/Checkout/steps/PaymentStep/PaymentStep.js
+++ app/components/Checkout/steps/PaymentStep/PaymentStep.js
@@ -28,7 +28,6 @@ class PaymentStep extends Component {
 		const { paymentData } = orderForm;
 		const paymentGroups = this.updatePaymentGroups(paymentData.paymentSystems);
 		const selectedPaymentGroupName = this.getDefaultPaymentGroup(paymentGroups);
-		console.log(paymentGroups);
 
 		this.state = {
 			paymentGroups,
